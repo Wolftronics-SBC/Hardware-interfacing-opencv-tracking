a898699f73b7501a5ca58eac2784fe0d8ba4b486
thorphar <info@harrythorpe.co.uk>
2017-10-20 16:47:59 +0100

    Follow Position and update hardware

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Compile and Run main.cpp
-
+./find.sh
 echo "Compiling Files...."
-g++ main.cpp -isystem /usr/include/boost -L/usr/include/ -lboost_system -std=gnu++11 -pthread -o test
+g++ main.cpp -isystem /usr/include/boost -L/usr/include/ -lboost_system `pkg-config --cflags --libs opencv` -std=gnu++11 -pthread -o test
 echo "Just created executable test ...."
 echo "Running ..."
 ./test
