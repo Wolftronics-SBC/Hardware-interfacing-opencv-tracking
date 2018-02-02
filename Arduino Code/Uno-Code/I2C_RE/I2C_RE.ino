fe11f0d71b75246936d57c4113f9d0b6679c4642
Harry Thorpe <info@harrythorpe.co.uk>
2018-02-02 11:19:09 +0000

    I2C commit for uno

---
+++ Arduino Code/Uno-Code/I2C_RE/I2C_RE.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+String data = "";
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(9);
+  Wire.onReceive(receiveEvent);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(data);
+}
+
+void receiveEvent(int howMany){
+  data = " ";
+  while(Wire.available()){
+      data += (char)Wire.read();
+      }
+}
