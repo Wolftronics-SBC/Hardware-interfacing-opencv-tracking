6e58bd0a5849a428efb400a8699e1c20fe7b4ad8
Harry Thorpe <hrrythorpe@googlemail.com>
2018-02-05 18:10:22 +0000

    Graph Fitting

--- Tests/MATLAB workspaces/Harry/plotdata.m
+++ Tests/MATLAB workspaces/Harry/plotdata.m
@@ -1,4 +1,4 @@
-function plotdata (sens1,sens2,ypos)
+function plotdata (ypos,sens1,sens2)
 
 sens1 = smooth(sens1,'moving');
 sens2 = smooth(sens2,'moving');
@@ -10,13 +10,21 @@ plot3(sens1,sens2,ypos); xlabel('Sensor 1'); ylabel('Sensor 2'); zlabel('Y Pos')
 figure;
 scatter3(sens1,sens2,ypos); xlabel('Sensor 1'); ylabel('Sensor 2'); zlabel('Y Pos'); grid on; 
 figure;
-surface(sens1,sens2,ypos); xlabel('Sensor 1'); ylabel('Sensor 2'); zlabel('Y Pos');
-shg;
 
 
+x1 = sens1(2108:4499);
+x2 = sens2(2108:4499);
+y = ypos(2108:4499);
 
+hold off;
+plot(y);
+hold on;
+plot(x1);
+plot(x2);
+legend('Y Pos','Sensor 1','Sensor 2');
+title('Filtered and banded');
 
-
+hold off;
 
 
 end
