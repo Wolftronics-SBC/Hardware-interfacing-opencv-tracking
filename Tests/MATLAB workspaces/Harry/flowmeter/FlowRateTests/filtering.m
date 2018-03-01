82e7a3290b89b49bbe1d4332d7c87c446367ba81
Harry Thorpe <hrrythorpe@googlemail.com>
2018-03-01 15:01:51 +0000

    filtering matlab

---
+++ Tests/MATLAB workspaces/Harry/flowmeter/FlowRateTests/filtering.m
@@ -0,0 +1,12 @@
+hold on;
+plot(lpm18psi30)
+plot(lpm20psi30)
+plot(lpm22psi30)
+plot(lpm25psi30)
+plot(lpm27psi40)
+plot(lpm30psi40)
+plot(lpm34psi50)
+legend('18-lpm 30-psi','20-lpm 30-psi','22-lpm 30-psi','25-lpm 30-psi',...
+    '27-lpm 40-psi','30-lpm 40-psi','34-lpm 50-psi');
+
+
